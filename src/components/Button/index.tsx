bd84bf8c0a658394925900e09a7f812005448e25
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-20 22:47:49 -0500

    Updated Theme + Added Confirmation Button Variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,56 @@
-import type { ButtonHTMLAttributes } from "react";
+import { type ButtonHTMLAttributes, type MouseEvent, useState, useRef, useEffect } from "react";
 import { theme } from "~/styles/theme.css";
-import { button, variants } from "~/components/Button/style.css";
-import clsx from "clsx";
+import { button, variants, bar, barVariants, content } from "~/components/Button/style.css";
 import { cva } from "class-variance-authority";
+import clsx from "clsx";
 
 export const style = cva(button, variants);
+export const progressStyle = cva(bar, barVariants);
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   size?: "small" | "large";
   variant?: "primary" | "secondary";
+  confirmation?: boolean;
   [key: string]: any;
 };
 
-const Button = ({ size, variant, className, ...intrinsic }: ButtonProps) => {
+const Button = ({ size, variant, className, confirmation, ...intrinsic }: ButtonProps) => {
+  const [isConfirming, setIsConfirming] = useState<boolean>(false);
+  const timeout = useRef<ReturnType<typeof setTimeout>>(null);
+
+  const onClick = (ev: MouseEvent<HTMLButtonElement>): void => {
+    if (!confirmation) {
+      intrinsic.onClick?.(ev);
+      return;
+    }
+
+    if (!isConfirming) {
+      setIsConfirming(true);
+      timeout.current = setTimeout(() => {
+        setIsConfirming(false);
+      }, 1500);
+    } else {
+      intrinsic.onClick?.(ev);
+      setIsConfirming(false);
+      if (timeout.current) {
+        clearTimeout(timeout.current);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeout.current) {
+        clearTimeout(timeout.current);
+      }
+    };
+  }, []);
+
   return (
-    <button { ...intrinsic } className={ clsx(theme, style({ size, variant }), className) } />
+    <button { ...intrinsic } onClick={ onClick } className={ clsx(theme, style({ size, variant }), className) }>
+      { isConfirming && <div className={ clsx(theme, progressStyle({ size, variant })) } /> }
+      <div className={ clsx(theme, content) } >{ intrinsic.children }</div>
+    </button>
   );
 };
 
